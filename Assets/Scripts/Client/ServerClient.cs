0d713cef2afc7bdd031be1a67eecead6e07d3785
kostya9 <kostya9@gmail.com>
2019-12-15 03:53:39 +0100

    Before upgrage

---
+++ Assets/Scripts/Client/ServerClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Client
+{
+    /*public class ServerClient : IDisposable
+    {
+        private readonly TcpClient _client;
+        private readonly NetworkStream _stream;
+
+        private ServerClient(TcpClient client)
+        {
+            _client = client;
+            _stream = _client.GetStream();
+            Task.Run(ReadData);
+        }
+
+        private async Task ReadData()
+        {
+            using(var reader = new BinaryReader(_stream))
+            while (!_client.Connected)
+            {
+                if (!_stream.DataAvailable)
+                {
+                    await Task.Delay(100);
+                    continue;
+                }
+
+                var s = reader.ReadString();
+                Debug.WriteLine(s);
+            }
+        }
+
+        public static async Task<ServerClient> Init(string address, int port)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            await socket.ConnectAsync(IPAddress.Parse(address), port);
+
+            var tcpClient = new TcpClient(address, port);
+
+            await tcpClient.ConnectAsync(address, port);
+
+            return new ServerClient(tcpClient);
+        }
+
+        public void Dispose()
+        {
+            _stream?.Dispose();
+            _client?.Dispose();
+        }
+    }*/
+}
---
+++ Assets/Tests/ServerTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ServerTest
+    {
+        // A Test behaves as an ordinary method
+        /*[Test]
+        public async Task ServerTestSimplePasses()
+        {
+            var client = await ServerClient.Init("127.0.0.1", 8080);
+            await Task.Delay(500);
+            client.Dispose();
+        }*/
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator ServerTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
