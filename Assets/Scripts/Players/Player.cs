a96ccff77215f27a019453d6cbea766a89350db7
Kostyantyn Sharovarsky <kostya9@gmail.com>
2019-12-18 21:24:46 +0100

    Draw planets in different colors and sizes (#5)

---
+++ Assets/Scripts/Players/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Player
+    {
+        public Player(int id, Color color)
+        {
+            Id = id;
+            Color = color;
+        }
+
+        public int Id { get; }
+
+        public Color Color { get; }
+    }
+}
